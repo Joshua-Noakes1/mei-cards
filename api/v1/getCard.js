27637644a7e4df45bafad5c58fa2e334f4247824
Joshua Noakes <joshua.noakes1@icloud.com>
2023-07-21 20:58:48 +0100

    fix names of images

--- api/v1/getCard.js
+++ api/v1/getCard.js
@@ -9,6 +9,9 @@ export default async function handler(req, res) {
     if (!game) {
       return res.status(400).json({ success: false, error: 'Missing game' });
     }
+    if (game != 'genshin' && game != 'honkai3rd' && game != 'starrail') {
+      return res.status(400).json({ success: false, error: 'Invalid game' });
+    }
 
     let hoyolabRecordCard = await getRecordCard(game);
     if (!hoyolabRecordCard.success) {
@@ -16,32 +19,40 @@ export default async function handler(req, res) {
     }
     console.log(hoyolabRecordCard);
 
-    // do canvas stuff
-    const canvas = createCanvas(300, 320)
-    const ctx = canvas.getContext('2d')
+    try {
+      // make canvas
+      const cardCanvas = createCanvas(1200, 600);
+      const cardCTX = cardCanvas.getContext('2d');
 
-    ctx.lineWidth = 10
-    ctx.strokeStyle = '#03a9f4'
-    ctx.fillStyle = '#03a9f4'
+      // load background based on game
 
-    // Wall
-    ctx.strokeRect(75, 140, 150, 110)
 
-    // Door
-    ctx.fillRect(130, 190, 40, 60)
+      // ctx.lineWidth = 10
+      // ctx.strokeStyle = '#03a9f4'
+      // ctx.fillStyle = '#03a9f4'
 
-    // Roof
-    ctx.beginPath()
-    ctx.moveTo(50, 140)
-    ctx.lineTo(150, 60)
-    ctx.lineTo(250, 140)
-    ctx.closePath()
-    ctx.stroke()
+      // // Wall
+      // ctx.strokeRect(75, 140, 150, 110)
 
-    res.setHeader('Cache-Control', 'public, max-age 432000, stale-while-revalidate 86400');
-    res.setHeader('Content-Type', 'image/png');
-    const pngCanvas = await canvas.encode('png');
-    return res.end(pngCanvas);
+      // // Door
+      // ctx.fillRect(130, 190, 40, 60)
+
+      // // Roof
+      // ctx.beginPath()
+      // ctx.moveTo(50, 140)
+      // ctx.lineTo(150, 60)
+      // ctx.lineTo(250, 140)
+      // ctx.closePath()
+      // ctx.stroke()
+
+      // res.setHeader('Cache-Control', 'public, max-age 432000, stale-while-revalidate 86400');
+      // res.setHeader('Content-Type', 'image/png');
+      // const pngCanvas = await canvas.encode('png');
+      // return res.end(pngCanvas);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ success: false, error: 'Failed to make card' });
+    }
   } catch (err) {
     console.log(err);
     return res.status(500).json({ success: false, error: 'Internal Server Error' });
